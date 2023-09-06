374b2ba8f1fbacd0f5ac126fb275cf521593eb2a
Gouri saini <gourisaini241@gmail.com>
2023-09-06 09:45:50 +0530

    first commit

---
+++ components/Gallery.js
@@ -0,0 +1,59 @@
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+import ReactPaginate from "react-paginate";
+
+const Gallery = () => {
+    const [pagecount, setPagecount] = useState(1);
+    const [images, setImages] = useState(null);
+
+    const GetImages = async () => {
+        const strdata = await fetch(
+            `https://picsum.photos/v2/list?page=${pagecount}&limit=10`
+        );
+        const json = await strdata.json();
+        setImages(json);
+    };
+
+    useEffect(() => {
+        GetImages();
+    }, [pagecount]);
+
+    const handlePageClick = (x) => {
+        setPagecount(x.selected + 1);
+    };
+
+    return (
+        <div>
+            <div className="d-flex flex-wrap">
+                {images
+                    ? images.map((image) => (
+                          <div key={image.id}>
+                              <img
+                                  src={image.download_url}
+                                  height="100"
+                                  alt=""
+                              />
+                              <Link href={`/card/${image.id}`}>
+                                  {image.author}
+                              </Link>
+                          </div>
+                      ))
+                    : "Loading..."}
+            </div>
+            {/* <button onClick={() => setPagecount(pagecount - 1)}>Prev</button>
+            <button onClick={() => setPagecount(pagecount + 1)}>Next</button> */}
+
+            {/*  */}
+            <ReactPaginate
+                nextLabel=">>"
+                onPageChange={handlePageClick}
+                pageCount={10}
+                previousLabel="<<"
+                renderOnZeroPageCount={null}
+            />
+            {/*  */}
+        </div>
+    );
+};
+
+export default Gallery;
--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import "@/node_modules/bootstrap/dist/css/bootstrap.min.css";
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
